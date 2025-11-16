9c559c0e62a3314363658b9bcd02d4e8d158b5fd
Alexis Boegner <alexisboegner@gmail.com>
2025-11-16 17:11:15 -0500

    Completed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> zelda = new List<string>
+            {
+                "Ocarina of Time", "Majora's Mask", "Twilight Princess", "Phantom Hourglass", "Skyward Sword",
+                "Breath of the Wild"
+            };
+            
+            List<string> zeldaAscendLength = zelda.OrderBy(x => x.Length).ToList();
+            zeldaAscendLength.ForEach(x => Console.WriteLine(x));
+            
+            List<string> zeldaDescendLength = zelda.OrderByDescending(x => x.Length).ToList();
+            zeldaDescendLength.ForEach(x => Console.WriteLine(x));
+            
+            List<string> zeldaUppercase = zelda.Select(x => x.ToUpper()).ToList();
+            zeldaUppercase.ForEach(x => Console.WriteLine(x));
         }
     }
 }
